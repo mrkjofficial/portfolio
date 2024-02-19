bcfeb0df831625178000fa490a4f3cf54c1b32f9
Karan Jaiswal <karanjaiswal0000@gmail.com>
2024-02-19 12:20:39 +0530

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_URL },
+					{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+					{ key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+					{ key: "X-Content-Type-Options", value: "nosniff" },
+					{ key: "X-Frame-Options", value: "DENY" },
+					{ key: "X-XSS-Protection", value: "1; mode=block" },
+					{ key: "Referrer-Policy", value: "same-origin" },
+				],
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
