cac8256494cf8395540639c3e6692b74152faff4
Karan Jaiswal <karanjaiswal0000@gmail.com>
2024-02-20 13:18:06 +0530

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,11 +9,16 @@ const nextConfig = {
 					{ key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_URL },
 					{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
 					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+				],
+			},
+			{
+				source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+				headers: [
 					{ key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
 					{ key: "X-Content-Type-Options", value: "nosniff" },
 					{ key: "X-Frame-Options", value: "DENY" },
 					{ key: "X-XSS-Protection", value: "1; mode=block" },
-					{ key: "Referrer-Policy", value: "same-origin" },
+					{ key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
 				],
 			},
 		];
