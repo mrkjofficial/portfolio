2d68d9b5ef719e8f2dd093608a7900e72abf79dd
Karan Jaiswal <karanjaiswal0000@gmail.com>
2024-02-20 15:53:20 +0530

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,11 +14,12 @@ const nextConfig = {
 			{
 				source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
 				headers: [
+					{ key: "Permissions-Policy", value: "browsing-topics=(), camera=(), geolocation=(), microphone=()" },
+					{ key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
 					{ key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
 					{ key: "X-Content-Type-Options", value: "nosniff" },
 					{ key: "X-Frame-Options", value: "DENY" },
 					{ key: "X-XSS-Protection", value: "1; mode=block" },
-					{ key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
 				],
 			},
 		];
