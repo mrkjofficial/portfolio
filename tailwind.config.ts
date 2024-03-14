08ead751d3a3aa3e22eeb18da7122d479b403ec3
Karan Jaiswal <karanjaiswal0000@gmail.com>
2024-03-15 00:06:55 +0530

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
-import { semanticColors } from "@nextui-org/theme";
 
 const config: Config = {
 	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
@@ -8,9 +7,6 @@ const config: Config = {
 	plugins: [nextui()],
 	theme: {
 		extend: {
-			backgroundImage: {
-				gradient: `linear-gradient(to right, ${semanticColors.light.primary[500]}, ${semanticColors.light.secondary[500]})`,
-			},
 			colors: {
 				light: "#F6FAFF",
 				dark: "#151515",
