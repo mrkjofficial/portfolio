48c4767292732d46079b438ed5a67d95acb385bd
Karan Jaiswal <karanjaiswal0000@gmail.com>
2025-09-01 01:48:55 +0530

    chore: update dependencies

--- app/hero.ts
+++ app/hero.ts
@@ -1,5 +1,60 @@
 import { heroui } from "@heroui/react";
 
-const Hero = heroui();
+const Hero = heroui({
+	themes: {
+		light: {
+			colors: {
+				background: "#F6FAFF",
+				foreground: "#151515",
+				primary: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#006FEE",
+				},
+				secondary: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#9353D3",
+				},
+				success: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#17C964",
+				},
+				danger: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#F31260",
+				},
+				warning: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#F5A524",
+				},
+			},
+		},
+		dark: {
+			colors: {
+				background: "#151515",
+				foreground: "#F6FAFF",
+				primary: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#006FEE",
+				},
+				secondary: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#9353D3",
+				},
+				success: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#17C964",
+				},
+				danger: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#F31260",
+				},
+				warning: {
+					foreground: "#F6FAFF",
+					DEFAULT: "#F5A524",
+				},
+			},
+		},
+	},
+});
 
 export default Hero;
